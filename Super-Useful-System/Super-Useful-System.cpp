d07fa677bbaf0e9ba9d7943a6dbbedc0fd3dc45d
Sany <Sany@BUK>
2024-04-19 23:16:19 +0300

    Добавьте файлы проекта.

---
+++ Super-Useful-System/Super-Useful-System.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class HelloWorld {
+private:
+    string m_verySuperMegaSecretString = "Hello World!";
+public:
+    void print() {
+        cout << m_verySuperMegaSecretString << endl;
+    }
+};
+
+int main()
+{
+    HelloWorld cat;
+    cat.print();
+}
